4f7f2af33712fe4368342d82ac78cfc862ef97b1
Shakhzod <shaxzodusmanov11@gmail.com>
2026-02-23 21:16:14 +0500

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import SpeedInsightsLoader from './components/SpeedInsightsLoader.jsx'
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <SpeedInsightsLoader />
+    <SpeedInsights />
   </StrictMode>,
 )
